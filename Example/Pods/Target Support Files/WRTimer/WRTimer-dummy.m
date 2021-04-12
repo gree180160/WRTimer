454f9b6cbb0031b2cd8b465111997aa15eff351e
gree180160 <1459287460@qq.com>
2021-04-12 16:49:28 +0800

    It's the first timer to add framework files.

---
+++ Example/Pods/Target Support Files/WRTimer/WRTimer-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_WRTimer : NSObject
+@end
+@implementation PodsDummy_WRTimer
+@end
--- Example/WRTimer/ViewController.swift
+++ Example/WRTimer/ViewController.swift
@@ -10,15 +10,55 @@ import UIKit
 
 class ViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
+    var timer: WRGCDTimerWrapper!
+       var numberValue: Int = 0
+       var lab: UILabel!
+       
+       deinit {
+           print("TestVC deinit")
+           guard timer != nil else {
+               return
+           }
+           timer.destroyTimer()
+       }
+       
+       override func viewDidLoad() {
+           super.viewDidLoad()
+           self.title = "test"
+           self.configureSubviews()
+       }
+       
+       func configureSubviews() {
+           lab = UILabel(frame: CGRect(x: 10, y: 200, width: self.view.frame.width - 20, height: 40))
+           lab.textAlignment = .center
+           self.view.addSubview(lab)
+           
+           timer = WRGCDTimerWrapper(target: self, timerAction: #selector(timerAction))
+           timer.fireTimer()
+           lab.text = "number is " + numberValue.description
+           
+           let button = UIButton(frame: CGRect(x: 100, y: 360, width: 140, height: 70))
+           button.setTitle("stop", for: .normal)
+           button.setTitle("fire", for: .selected)
+           button.backgroundColor = .brown
+           self.view.addSubview(button)
+           button.addTarget(self, action: #selector(buttonAction(sender:)), for: .touchUpInside)
+       }
+       
+       @objc func buttonAction(sender: UIButton) {
+           if sender.isSelected {
+               timer.fireTimer()
+           }else {
+               timer.stopTimer()
+           }
+           sender.isSelected = !sender.isSelected
+       }
+       
+       
+       @objc func timerAction() {
+           numberValue += 1
+           lab.text = "number is " + numberValue.description
+       }
 
 }
 
